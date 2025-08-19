a3acf90aab06d90a6c161a835992acec2cce77ab
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-19 21:40:10 +0545

    updateNote() method created

--- 10Note-App_LocalStorage/src/App.jsx
+++ 10Note-App_LocalStorage/src/App.jsx
@@ -10,11 +10,11 @@ function App() {
     setNotes((prev) => [{id: Date.now(), ...note}, ...prev])
   }
 
-  // const updatedNote = (id, note) =>{
-  //   setNotes((prev) =>
-  //   prev.map((prevNote) =>
-  //   prevNote.id === id ? note : prevNote))
-  // }
+  const updatedNote = (id, note) =>{
+    setNotes((prev) =>
+    prev.map((prevNote) =>
+    prevNote.id === id ? note : prevNote))
+  }
 
   // const deleteNote = (id) =>{
   //   setNotes((prev) => prev.filter((note) => note.id !== id))
