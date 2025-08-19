f5036ea32fd60a2e681c9075ff2ba7fd0ce8489d
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-19 20:37:16 +0545

    appjsxx edited

--- 10Note-App_LocalStorage/src/App.jsx
+++ 10Note-App_LocalStorage/src/App.jsx
@@ -11,3 +11,29 @@ function App() {
 }
 
 export default App
+
+
+
+// 🔹 1. Notes App
+
+// Each note = { id, title, description }
+
+// Features:
+
+// Add new note (title + description).
+
+// Edit a note.
+
+// Delete a note.
+
+// Save/load from LocalStorage.
+
+// Context API methods:
+
+// addNote(note)
+
+// updateNote(id, updatedNote)
+
+// deleteNote(id)
+
+// 👉 Data flow is almost identical to your Todo App, except now you have two fields instead of one (title, description).
